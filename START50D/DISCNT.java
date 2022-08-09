398a6ececaa48c4828cf4ce949b93d091af2060f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-08-09 15:12:21 +0530

    START50D : DISCNT
    Problem link to try it Yourself : https://www.codechef.com/START50D/problems/DISCNT

---
+++ START50D/DISCNT.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        try {
+            Scanner sc = new Scanner(System.in);
+            int test = sc.nextInt();
+            while(test-->0)
+            {
+                int dis = sc.nextInt();
+                System.out.println(100-dis);
+            }
+            
+        } catch(Exception e) {
+            return;
+        }
+    }
+}
