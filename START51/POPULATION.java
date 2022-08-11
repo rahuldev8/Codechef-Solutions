cf62bfd15f6b8282414383421d81d77a9b94c2d2
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-08-11 21:54:28 +0530

    START52 :  POPULATION
    Problem Link to try it Yourself : https://www.codechef.com/START51D/problems/POPULATION

---
+++ START51/POPULATION.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        try {
+            Scanner sc = new Scanner(System.in);
+            int test = sc.nextInt();
+            while(test-->0)
+            {
+                int x =sc.nextInt();
+                int y = sc.nextInt();
+                int z = sc.nextInt();
+                System.out.println(x-y+z);
+            }
+            
+        } catch(Exception e) {
+            return;
+        }
+    }
+}
